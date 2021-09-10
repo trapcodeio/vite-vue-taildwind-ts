87c0508926ff4b8020d078d32ed5194a5b5b7b89
trapcodeio <trapcode.io@gmail.com>
2021-09-10 15:08:27 +0100

    Remove Vue Ejs Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteEjsPlugin()]
+  plugins: [vue()]
 });
